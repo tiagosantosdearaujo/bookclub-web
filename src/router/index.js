609fc8ca55bf949ae79493f99fe8f564d90f2b09
Tiago Araujo <tiago.tsa1@hotmail.com>
2023-03-04 07:06:54 -0800

    feat: Global state implemented

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import { unauthRoutes } from "modules/unauthenticated/routes";
+import { authRoutes } from "modules/authenticaded/routes";
 
-export const router = createBrowserRouter([...unauthRoutes]);
+export const router = createBrowserRouter([...unauthRoutes, ...authRoutes]);
